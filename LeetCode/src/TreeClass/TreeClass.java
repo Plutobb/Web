04f334e5e8469093d89f481141409f53db99d309
Plutobb <1194839756@qq.com>
2020-09-27 17:44:38 +0800

    二叉树问题

--- LeetCode/src/TreeClass/TreeClass.java
+++ LeetCode/src/TreeClass/TreeClass.java
@@ -10,6 +10,13 @@ public class TreeClass {
         TreeNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    '}';
+        }
     }
     static LinkedList<Integer> list = new LinkedList();
     static int maxCount = 0;
@@ -66,14 +73,54 @@ public class TreeClass {
         findPathHelper(root.right,sum,pathSumPart,pathSumAns);
         pathSumPart.remove(pathSumPart.size()-1);
     }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null){
+            return null;
+        }
+        if (root == p){
+            return root;
+        }
+        if (root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null){
+            return root;
+        }else if (left != null){
+            return left;
+        }else {
+            return right;
+        }
+    }
+    public static TreeNode lowestCommonAncestorMethod2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < q.val && root.val <p.val){
+            return lowestCommonAncestorMethod2(root.right,p,q);
+        }else if (root.val > q.val && root.val > p.val){
+            return lowestCommonAncestorMethod2(root.left,p,q);
+        }else {
+            return root;
+        }
+    }
     public static void main(String[] args) {
-        TreeNode a = new TreeNode(5);
-        TreeNode b = new TreeNode(4);
-        TreeNode c = new TreeNode(11);
-        TreeNode d = new TreeNode(2);
-        a.right = b;
-        b.left = c;
-        c.left = d;
-        System.out.println(pathSum(a,22));
+        TreeNode a = new TreeNode(6);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(8);
+        TreeNode d = new TreeNode(0);
+        TreeNode e = new TreeNode(4);
+        TreeNode f = new TreeNode(3);
+        TreeNode g = new TreeNode(5);
+        TreeNode h = new TreeNode(7);
+        TreeNode i = new TreeNode(9);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        e.left = f;
+        e.right = g;
+        c.left = h;
+        c.right = i;
+        System.out.println(lowestCommonAncestorMethod2(a,a,h));
     }
 }
