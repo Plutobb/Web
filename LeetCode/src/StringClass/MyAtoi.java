7f2f6f8a1ac5b2485064b775da08c548747899d5
Plutobb <1194839756@qq.com>
2020-10-12 21:52:36 +0800

    字符串转换整数

---
+++ LeetCode/src/StringClass/MyAtoi.java
@@ -0,0 +1,63 @@
+package StringClass;
+
+import javax.security.auth.login.CredentialException;
+
+public class MyAtoi {
+    public static int myAtoi(String s) {
+        int i;
+        int flag = 1;
+        StringBuilder sb = new StringBuilder("");
+        for (i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' '){
+                continue;
+            }else {
+                break;
+            }
+        }
+        if (i == s.length()){
+            return 0;
+        }else {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-' || (s.charAt(i) >= '0' && s.charAt(i) <= '9')){
+                if (s.charAt(i) == '-'){
+                    flag = -1;
+                }
+                sb.append(s.charAt(i));
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(j) >= '0' && s.charAt(j) <= '9'){
+                        sb.append(s.charAt(j));
+                    }else {
+                        break;
+                    }
+                }
+            }else {
+                return 0;
+            }
+        }
+        long ans;
+        try {
+            ans = Long.parseLong(sb.toString());
+            if (ans > Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if (ans < Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+        } catch (NumberFormatException e) {
+            if (sb.length() ==1){
+                return 0;
+            }else {
+                if (flag == 1){
+                    return Integer.MAX_VALUE;
+                }else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+        return (int)ans;
+    }
+
+    public static void main(String[] args) {
+        String string = "2000000000000000000000";
+        System.out.println(myAtoi(string));
+    }
+}
