68f7e444bb9b9587d84eccbf8a6bc1b4b03a6da8
Plutobb <1194839756@qq.com>
2020-11-22 23:32:14 +0800

    类加载器测试

---
+++ JVM/src/CLassLoaderTest.java
@@ -0,0 +1,7 @@
+public class CLassLoaderTest {
+    public static void main(String[] args) {
+        System.out.println(ClassLoader.getSystemClassLoader());
+        System.out.println(ClassLoader.getSystemClassLoader().getParent());
+        System.out.println(ClassLoader.getSystemClassLoader().getParent().getParent());
+    }
+}
