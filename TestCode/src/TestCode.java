e2131886ccff626cc6b4b184f607d589aaea577f
Plutobb <1194839756@qq.com>
2020-12-02 17:23:38 +0800

    代码测试

---
+++ TestCode/src/TestCode.java
@@ -0,0 +1,8 @@
+public class TestCode {
+    public static void main(String[] args) {
+        short a = 128;
+        byte b = (byte) a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
