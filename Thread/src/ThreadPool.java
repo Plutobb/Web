19e0623f16d7839ccf5c1ab41411ff62b6c3dfa8
Plutobb <1194839756@qq.com>
2021-01-30 18:47:17 +0800

    线程池

--- Thread/src/ThreadPool.java
+++ Thread/src/ThreadPool.java
@@ -1,19 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPool {
 
     public static Runnable getThread(int i){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         return new Runnable() {
             @Override
             public void run() {
-                System.out.println("任务"+ i +"开始");
+                System.out.println("任务"+ i +"开始! " + sdf.format(new Date()));
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("任务"+i+"结束!");
+                System.out.println("任务"+i+"结束! " + sdf.format(new Date()));
             }
         };
 
@@ -23,8 +28,10 @@ public class ThreadPool {
         ExecutorService fixPool = Executors.newFixedThreadPool(5);
         ExecutorService singlePool = Executors.newSingleThreadExecutor();
         ExecutorService cachedPool = Executors.newCachedThreadPool();
-        for (int i = 0; i < 10; i++) {
-            cachedPool.execute(getThread(i));
+        ScheduledExecutorService sChePool = Executors.newScheduledThreadPool(1);
+        for (int i = 0; i < 3; i++) {
+            //延迟0秒,每隔5秒任务周期执行.
+            sChePool.scheduleWithFixedDelay(getThread(i),0,5, TimeUnit.SECONDS);
         }
     }
 }
