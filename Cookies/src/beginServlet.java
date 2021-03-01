7fa6cc87c13abc139426bab3ed6304c163d4e892
Plutobb <1194839756@qq.com>
2021-03-01 23:00:11 +0800

    Cookies练习---订餐系统

---
+++ Cookies/src/beginServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class beginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName,balance;
+        userName = request.getParameter("userName");
+        balance = request.getParameter("balance");
+
+        Cookie card1 = new Cookie("userName",userName);
+        Cookie card2 = new Cookie("balance",balance);
+
+        response.addCookie(card1);
+        response.addCookie(card2);
+
+    }
+}
