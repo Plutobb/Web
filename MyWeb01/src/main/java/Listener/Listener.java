2c312bf395e8253bb10ecb4ab0641441bb13107b
Plutobb <1194839756@qq.com>
2021-03-05 12:34:17 +0800

    监听器提高运行速度

---
+++ MyWeb01/src/main/java/Listener/Listener.java
@@ -0,0 +1,14 @@
+package Listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class Listener implements HttpSessionListener {
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+    }
+
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+    }
+}
---
+++ MyWeb01/src/main/java/filter/filter.java
@@ -0,0 +1,19 @@
+package filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class filter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
