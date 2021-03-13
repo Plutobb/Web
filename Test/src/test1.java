b79a722cbc3961642c5ae517640798490521c5c6
Plutobb <1194839756@qq.com>
2021-03-13 20:00:58 +0800

    测试

--- Test/src/test1.java
+++ Test/src/test1.java
@@ -1,7 +1,7 @@
 public class test1 {
-    static {
-        System.out.println("外部类静态代码块");
-    }
+//    static {
+//        System.out.println("外部类静态代码块");
+//    }
 
 
     static class InnerStaticClass {
@@ -11,11 +11,24 @@ public class test1 {
         }
     }
 
+    public static synchronized void print(int n){
+        System.out.println(n);
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
         //test1 test1 = new test1();
-        InnerStaticClass innerStaticClass = new InnerStaticClass();
-        System.out.println(ClassLoader.getSystemClassLoader());
-        Class<?> aClass = Class.forName("InnerStaticClass");
+//        InnerStaticClass innerStaticClass = new InnerStaticClass();
+//        System.out.println(ClassLoader.getSystemClassLoader());
+//        Class<?> aClass = Class.forName("InnerStaticClass");
+        Thread thread1 = new Thread(()->{
+            print(1);
+        });
+        Thread thread2 = new Thread(()->{
+            print(2);
+        });
+        Thread thread3 = new Thread(()->{
+           print(3);
+        });
     }
 
 }
