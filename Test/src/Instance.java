1f522e25c54a5bccec5b0495a1f91885be8b1153
Plutobb <1194839756@qq.com>
2021-03-17 21:22:19 +0800

    单例

--- Test/src/Instance.java
+++ Test/src/Instance.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Instance {
     static Instance instance = new Instance();
@@ -11,11 +12,25 @@ public class Instance {
 
     @lombok.SneakyThrows
     public static void main(String[] args) {
-        Instance instance = new Instance();
-        Class<? extends Instance> aClass = instance.getClass();
-        Constructor<? extends Instance> instance1 = aClass.getDeclaredConstructor(null);
-        instance1.setAccessible(true);
-        Instance instance2 = instance1.newInstance();
-        System.out.println(instance == instance2);
+        try {
+            Instance instance = new Instance();
+            Class<? extends Instance> aClass = instance.getClass();
+            Constructor<? extends Instance> instance1 = aClass.getDeclaredConstructor(null);
+            instance1.setAccessible(true);
+            Instance instance2 = instance1.newInstance();
+            System.out.println(instance == instance2);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
