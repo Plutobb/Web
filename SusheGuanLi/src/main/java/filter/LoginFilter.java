b831bb80218faf4bd082ccdb88f3593c8e347517
Plutobb <1194839756@qq.com>
2021-03-19 18:46:02 +0800

    宿舍管理系统

--- SusheGuanLi/src/main/java/filter/LoginFilter.java
+++ SusheGuanLi/src/main/java/filter/LoginFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebFilter("/*")
+@WebFilter("/main")
 public class LoginFilter implements Filter {
 
     @Override
